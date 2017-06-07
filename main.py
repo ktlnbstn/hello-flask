4c4b322ec2792909f6516bd263d53199b8888e65
Kate Boston <ktlnbstn@gmail.com>
2017-06-06 21:51:03 -0700

    addition to html validation/redirect/escaping

---
+++ main.py
@@ -0,0 +1,97 @@
+from flask import Flask, request, redirect
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+<html>
+    <body>
+        <form action="/hello" method="POST">
+            <label for="first_name">First Name: </label>
+            <input id="first_name" type="text" name="first_name" />
+            <input type="submit" />
+        </form>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return form
+
+@app.route("/hello", methods=["POST"])
+def hello():
+    first_name = request.form['first_name']
+    return '<h1>Hello, ' + cgi.escape(first_name) + '</h1>'
+
+time_form = """
+    <style>
+        .error {{ color: red; }}
+    </style>
+    <h1>Validate Time</h1>
+    <form action="/validate-time" method="POST">
+        <label>Hours (24-hour format)
+            <input name="hours" type="text" value="{hours}" >
+        </label>
+        <p class="error">{hours_error}</p>
+        <label>Minutes
+            <input name="minutes" type="text" value="{minutes}" />
+        </label>
+        <p class="error">{minutes_error}</p>
+        <input type="submit" value="Validate" />
+    </form>
+    """
+
+@app.route('/validate-time')
+def display_time_form():
+    return time_form.format(hours="", hours_error="", minutes="", minutes_error="")
+
+def is_integer(num):
+    try:
+        int(num)
+        return True
+    except ValueError:
+        return False
+
+@app.route('/validate-time', methods=['POST'])
+def validate_time():
+
+    hours = request.form['hours']
+    minutes = request.form['minutes']
+
+    hours_error = ""
+    minutes_error = ""
+
+    if not is_integer(hours):
+        hours_error = 'Not a valid integer'
+        hours = ''
+    else:
+        hours = int(hours)
+        if hours > 23 or hours < 0:
+            hours_error = 'Hour value out of range (0-23)'
+            hours = ''
+
+    if not is_integer(minutes):
+        minutes_error = 'Not a valid integer'
+        minute = ''
+    else:
+        minutes = int(minutes)
+        if minutes > 59 or minutes < 0:
+            minutes_error = 'Minutes vaule out of range (0-59)'
+            minutes = ''
+
+    if not minutes_error and not hours_error:
+        time = str(hours) + ':' + str(minutes)
+        return redirect('/valid-time?time={0}'.format(time))
+    else:
+        return time_form.format(hours_error=hours_error, minutes_error=minutes_error,
+        hours=hours, minutes=minutes)
+
+@app.route('/valid-time')
+def vaild_time():
+    time = request.args.get('time')
+    return '<h1>You submiteed {0}. Thanks for submitting a valid time!</h1>'.format(time)
+
+app.run()
